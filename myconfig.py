b08a17134bd3e258cb2dd79a26d5c7a1e7920681
JosefGst <61586098+JosefGst@users.noreply.github.com>
2021-02-26 08:25:40 +0800

    myconfig changes

--- myconfig.py
+++ myconfig.py
@@ -20,34 +20,34 @@
 # MODELS_PATH = os.path.join(CAR_PATH, 'models')
 
 # #VEHICLE
-# DRIVE_LOOP_HZ = 20      # the vehicle loop will pause if faster than this speed.
+DRIVE_LOOP_HZ = 70 #20      # the vehicle loop will pause if faster than this speed.
 # MAX_LOOPS = None        # the vehicle loop can abort after this many iterations, when given a positive integer.
 
 # #CAMERA
 # CAMERA_TYPE = "CSIC"   # (PICAM|WEBCAM|CVCAM|CSIC|V4L|D435|MOCK|IMAGE_LIST), We use CSIC in this competition.
-# IMAGE_W = 224
-# IMAGE_H = 224
+# IMAGE_W = 112 #224
+# IMAGE_H = 112 #224
 # IMAGE_DEPTH = 3         # default RGB=3, make 1 for mono
 # CAMERA_FRAMERATE = DRIVE_LOOP_HZ
 
 # # Region of interst cropping
-ROI_CROP_TOP = 35                    #the number of rows of pixels to ignore on the top of the image
-ROI_CROP_BOTTOM = 10                 #the number of rows of pixels to ignore on the bottom of the image
+ROI_CROP_TOP = 50       #35                    #the number of rows of pixels to ignore on the top of the image
+ROI_CROP_BOTTOM = 70    #10             #the number of rows of pixels to ignore on the bottom of the image
 
 # #9865, over rides only if needed, ie. TX2..
 # PCA9685_I2C_ADDR = 0x40     #I2C address, use i2cdetect to validate this number
 # PCA9685_I2C_BUSNUM = 1   #None will auto detect, which is fine on the pi. But other platforms should specify the bus num.
 
 # #STEERING
-# STEERING_CHANNEL = 0            #channel on the 9685 pwm board 0-15
-STEERING_LEFT_PWM = 460         #pwm value for full left steering
-STEERING_RIGHT_PWM = 290        #pwm value for full right steering
+#STEERING_CHANNEL = 0            #channel on the 9685 pwm board 0-15
+STEERING_LEFT_PWM = 505         #pwm value for full left steering
+STEERING_RIGHT_PWM = 275        #pwm value for full right steering
 
 #THROTTLE
-# THROTTLE_CHANNEL = 1            #channel on the 9685 pwm board 0-15
-# THROTTLE_FORWARD_PWM = 435      #pwm value for max forward throttle, default: 435
+#THROTTLE_CHANNEL = 1            #channel on the 9685 pwm board 0-15
+THROTTLE_FORWARD_PWM = 500      #pwm value for max forward throttle, default: 435
 # THROTTLE_STOPPED_PWM = 370      #pwm value for no movement, default: 370
-# THROTTLE_REVERSE_PWM = 330      #pwm value for max reverse throttle, default: 330
+THROTTLE_REVERSE_PWM = 330      #pwm value for max reverse throttle, default: 330
 
 # #STEERING FOR PIGPIO_PWM
 # STEERING_PWM_PIN = 13           #Pin numbering according to Broadcom numbers
@@ -73,23 +73,24 @@ STEERING_RIGHT_PWM = 290        #pwm value for full right steering
 # PRINT_MODEL_SUMMARY = True      #print layers and weights to stdout
 # LEARNING_RATE = 0.0001           #only used when OPTIMIZER specified
 # NUM_WORKERS = 8                 # Setting the argument num_workers as a positive integer will turn on multi-process data loading with the specified number of loader worker processes.
-# COLOR_JITTER_TRANSFORMS = True  #weather to add color noise (brightness, contrast, ...) during training.
+COLOR_JITTER_TRANSFORMS = True  #weather to add color noise (brightness, contrast, ...) during training.
 
 # #WEB CONTROL
 # WEB_CONTROL_PORT = 8887             # which port to listen on when making a web controller
 # WEB_INIT_MODE = "user"              # which control mode to start in. one of user|local_angle|local. Setting local will start in ai mode.
 
 # #JOYSTICK
-# USE_JOYSTICK_AS_DEFAULT = False     #when starting the manage.py, when True, will not require a --js option to use the joystick
-JOYSTICK_MAX_THROTTLE = 0.4         # [0,1], this scalar is multiplied with the throttle value (-1 to 1) to limit the maximum throttle. This can help if you drop the controller or just don't need the full speed available.
-# JOYSTICK_STEERING_SCALE = 1.0       #some people want a steering that is less sensitve. This scalar is multiplied with the steering -1 to 1. It can be negative to reverse dir.
-# AUTO_RECORD_ON_THROTTLE = True      #if true, we will record whenever throttle is not zero. if false, you must manually toggle recording with some other trigger. Usually circle button on joystick.
+CONTROLLER_TYPE = 'custom'
+# USE_JOYSTICK_AS_DEFAULT = True     #when starting the manage.py, when True, will not require a --js option to use the joystick
+JOYSTICK_MAX_THROTTLE = .75         # [0,1], this scalar is multiplied with the throttle value (-1 to 1) to limit the maximum throttle. This can help if you drop the controller or just don't need the full speed available.
+#JOYSTICK_STEERING_SCALE = 1       #some people want a steering that is less sensitve. This scalar is multiplied with the steering -1 to 1. It can be negative to reverse dir.
+AUTO_RECORD_ON_THROTTLE = False      #if true, we will record whenever throttle is not zero. if false, you must manually toggle recording with some other trigger. Usually circle button on joystick.
 # JOYSTICK_DEADZONE = 0.0             # when non zero, this is the smallest throttle before recording triggered.
 # JOYSTICK_THROTTLE_DIR = -1.0        # use -1.0 to flip forward/backward, use 1.0 to use joystick's natural forward/backward
-# USE_FPV = False                     # send camera data to FPV webserver
+USE_FPV = False                     # send camera data to FPV webserver
 # JOYSTICK_DEVICE_FILE = "/dev/input/js0" # this is the unix file use to access the joystick.
-GENTLE_THROTTLE = 0.4
-RAGE_THROTTLE = 0.7
+#GENTLE_THROTTLE = 0.40
+# RAGE_THROTTLE = 0.75
 # PER_THROTTLE_STEP = 0.05
 
 # #RNN or 3D
@@ -99,13 +100,13 @@ RAGE_THROTTLE = 0.7
 # RECORD_DURING_AI = False        #normally we do not record during ai mode. Set this to true to get image and steering records for your Ai. Be careful not to use them to train.
 
 # #Scale the output of the throttle of the ai pilot for all model types.
-# AI_THROTTLE_MULT = 1.0              # this multiplier will scale every throttle value for all output from NN models
+AI_THROTTLE_MULT = 0.95             # this multiplier will scale every throttle value for all output from NN models
 # AI_MAX_THROTTLE = 0.75         # the throttle upper bound during AI mode, in case the model outputs a too large values leading the car to drive too fast and become unstable.
-# AI_MIN_THROTTLE = 0.1          # the throttle lower bound during AI mode, in case the model outputs a too small values leading the car to stop
+AI_MIN_THROTTLE = 0.3    #0.1      # the throttle lower bound during AI mode, in case the model outputs a too small values leading the car to stop
 
 # # Whether or not add extra control noise during user mode
 # # IF set to TRUE, random action noise (on steering angle and throttle) will be added during your tele-operation. 
 # # This can help you to collect more data that the car recovers from off-center and off-orientation mistakes, and then your trained agent will be more "intelligent"
 # CONTROL_NOISE = True
-# THROTTLE_NOISE = 0.05          # should be > 0, then the range of throttle noise will be in [-THROTTLE_NOISE, THROTTLE_NOISE] (uniform random sampler), larger value leads to more difficult data collection
-# ANGLE_NOISE = 0.25             # should be > 0, then the range of steering noise will be in [-ANGLE_NOISE, ANGLE_NOISE] (uniform random sampler), larger value leads to more difficult data collection
+#THROTTLE_NOISE = 0.05          # should be > 0, then the range of throttle noise will be in [-THROTTLE_NOISE, THROTTLE_NOISE] (uniform random sampler), larger value leads to more difficult data collection
+#ANGLE_NOISE = 0.25             # should be > 0, then the range of steering noise will be in [-ANGLE_NOISE, ANGLE_NOISE] (uniform random sampler), larger value leads to more difficult data collection
